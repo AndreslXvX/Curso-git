a443a96e341394be5937daaa72f0f590a8408ba5
AndreslXvX <eduardocondealvarez@2006gmail.com>
2024-09-27 15:13:21 -0600

    algo bien

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("oolv")
+let respuesta = ""
+let resultado = 0
+
+for (let i = 1; i !== resultado ; i++) {
+        if (respuesta !== "si") {
+        respuesta = prompt("te gusta el pilin?") 
+        console.log(respuesta)
+        } else {
+            alert("ya sabia xd")
+            resultado = i + 1
+        }
+}
+
+
